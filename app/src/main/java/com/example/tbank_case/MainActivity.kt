050a1c32ba717295ad3ae0e6758f79e4af4fc442
k0ist <arseniy.ershov.1206@mail.ru>
2024-10-21 01:13:10 +0300

    LAST UPDATE 21.10.2024

--- app/src/main/java/com/example/tbank_case/MainActivity.kt
+++ app/src/main/java/com/example/tbank_case/MainActivity.kt
@@ -1,20 +1,69 @@
 package com.example.tbank_case
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import java.util.Locale
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+interface Animal {
+    val weight: Double
+    val age: Int
+}
+
+
+enum class Bite {
+    STRAIGHT,
+    OVER,
+    UNDER
+}
+
+
+enum class Behavior {
+    ACTIVE,
+    PASSIVE
+}
+
+interface Dog: Animal {
+    val bite: Bite
+}
+
+
+interface Cat: Animal {
+    val behavior: Behavior
+}
+
+
+data class Husky(override val weight: Double, override val age: Int) : Dog {
+    override val bite = Bite.STRAIGHT
+}
+
+
+data class Corgi(override val weight: Double, override val age: Int) : Dog {
+    override val bite = Bite.UNDER
+}
+
+
+data class ScottishCat(override val weight: Double, override val age: Int) : Cat {
+    override val behavior = Behavior.PASSIVE
+}
+
+
+data class SiameseCat(override val weight: Double, override val age: Int) : Cat {
+    override val behavior = Behavior.ACTIVE
+}
+
+
+class PetShop {
+    fun idAnimal(breed: String): String {
+        return when (breed.lowercase(Locale.getDefault())) {
+            "husky" -> "Dog"
+            "corgi" -> "Dog"
+            "scottish" -> "Cat"
+            "siamese" -> "Cat"
+            else -> "Unknown animal"
         }
     }
+}
+
+
+fun main() {
+
 }
